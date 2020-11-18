425bc23c87f7798435d3ac554fd56f05d4e3dce0
e-r-ik <44287767+e-r-ik@users.noreply.github.com>
2020-11-18 21:16:53 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,58 @@
+var freezegun = {name: "freezegun", price: "69$", picurl: "freezeraygru.png", href: "freezegun.html"};
+var shrinkray = {name: "shrinkray", price: "69$", picurl: "shrinkray.jpg", href: "shrinkray.html"};
+var fartgun = {name: "fartgun", price: "69$", picurl: "fartgun.png", href: "fartgun.html"};
+var tranquilizerdarts = {name: "tranquilizer", price: "69$", picurl: "tranquilizer.png" , href: "transquilizer.html"};
+var zapplistick = {name: "zapplipstick", price: "69$", picurl: "lipsticktazer.png", href: "lipsticktazer.html"};
+var piranhagun = {name: "piranhagun", price: "69$", picurl: "piranhagun.png", href: "piranhagun.html"};
+
+var grumobile = {name: "grumobile", price: "69$", picurl: "grumobile.png", href: "grumobile.html"};
+var gruairship = {name: "gruairship", price: "69$", picurl: "gruairship.png", href: "gruairship.html"};
+var scooter = {name: "scooter", price: "69$", picurl: "scooter.png", href: "scooter.html"};
+var drucar = {name: "drucar", price: "69$", picurl: "drucar.png", href: "drucar.html"};
+
+var hitsquad1 = {name: "hitsquad1", price: "69$", picurl: "hitsquad.png"};
+
+var all = [freezegun, shrinkray, fartgun, tranquilizerdarts,
+           zapplistick, piranhagun, grumobile, gruairship, scooter, drucar];
+var weapons = [freezegun, shrinkray, fartgun, tranquilizerdarts, zapplistick, piranhagun];
+var vehicles = [grumobile, gruairship, scooter, drucar];
+var hitsquads = [hitsquad1];
+
+function showAll() {
+  for (var i = 0; i < all.length; i++) {
+    var a = document.createElement("a");
+    var pname = document.createElement("p");
+    var pprice = document.createElement("p");
+    var b = document.getElementsByClassName("productList");
+    var textnodepname = document.createTextNode(all[i].name);
+    var textnodepprice = document.createTextNode(all[i].price);
+    var ahref = document.createAttribute("href");
+    var aclass = document.createAttribute("class");
+    var pnameclass = document.createAttribute("class");
+    var ppriceclass = document.createAttribute("class");
+
+    a.appendChild(pname);
+    a.appendChild(pprice);
+    a.setAttributeNode(ahref);
+    a.setAttributeNode(aclass);
+
+    ahref.value = all[i].href;
+    aclass.value = "productitem";
+
+    pname.appendChild(textnodepname);
+    pname.setAttributeNode(pnameclass);
+
+    pnameclass.value = "";
+
+    pprice.appendChild(textnodepprice);
+    pprice.setAttributeNode(ppriceclass);
+
+    ppriceclass.value = "";
+
+
+
+    b.appendChild(a);
+  }
+}
+
+showAll();
