680d5a86476e78ecb8f5500230ba189b3e5e7808
e-r-ik <44287767+e-r-ik@users.noreply.github.com>
2020-11-18 21:16:57 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,25 +1,28 @@
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// Apply to all slideshows that you define with the markup wrote
-slideshows.forEach(initSlideShow);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-function initSlideShow(slideshow) {
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
-
-  var index = 0, time = 5000;
-  slides[index].classList.add('active');
-
-  setInterval( () => {
-    slides[index].classList.remove('active');
-
-    //Go over each slide incrementing the index
-    index++;
-
-    // If you go over all slides, restart the index to show the first slide and start again
-    if (index === slides.length) index = 0;
-
-    slides[index].classList.add('active');
-
-  }, time);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
